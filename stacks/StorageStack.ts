ac31243d249c4da9828a000739acdc45be40bb05
Matthew Wyskiel <developer@mattwyskiel.com>
2023-05-24 10:58:53 -0400

    Version the bucket!

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -3,6 +3,11 @@ import { StackContext, Bucket } from 'sst/constructs';
 export function Storage({ stack }: StackContext) {
   const bucket = new Bucket(stack, 'DocumentBucket', {
     name: stack.stage === 'prod' ? 'mattwyskiel-documents' : undefined,
+    cdk: {
+      bucket: {
+        versioned: true,
+      },
+    },
   });
   return {
     bucket,
