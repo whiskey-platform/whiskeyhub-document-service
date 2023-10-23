83ffa13ecc18ea2eee92c9db3434d253c4b71c0b
Matthew Wyskiel <matt@mattwyskiel.com>
2023-10-23 00:42:46 -0400

    Amend list files function to be not grouped

--- packages/functions/src/list-files/function.ts
+++ packages/functions/src/list-files/function.ts
@@ -7,11 +7,22 @@ import responseMonitoring from '../lib/middleware/response-monitoring';
 const s3: IS3Service = new S3Service();
 
 const listFiles: APIGatewayProxyHandlerV2 = async event => {
-  const { objects, folders } = await s3.retrieveGroupedObjects(
-    Bucket.DocumentBucket.bucketName,
-    event.queryStringParameters ? event.queryStringParameters!['prefix'] : undefined
-  );
-  return json({ objects, folders });
+  if (event.queryStringParameters?.notGrouped === 'true') {
+    return {
+      body: JSON.stringify(
+        await s3.retrieveObjects(
+          Bucket.DocumentBucket.bucketName,
+          event.queryStringParameters?.prefix
+        )
+      ),
+    };
+  } else {
+    const { objects, folders } = await s3.retrieveGroupedObjects(
+      Bucket.DocumentBucket.bucketName,
+      event.queryStringParameters ? event.queryStringParameters!['prefix'] : undefined
+    );
+    return json({ objects, folders });
+  }
 };
 
 export const handler = wrapped(listFiles).use(responseMonitoring());
